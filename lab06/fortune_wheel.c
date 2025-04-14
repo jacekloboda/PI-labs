53c27bf085cded05ae1fc4361d41c26251119560
jacekloboda <jacekloboda55@gmail.com>
2025-04-14 15:19:09 +0200

    fix fortune_wheel.c score handling

--- lab06/fortune_wheel.c
+++ lab06/fortune_wheel.c
@@ -83,13 +83,13 @@ const char* generate_word(void)
     return words[ind];
 }
 
-void check_char(const char *word, int *word_len, int *shown_letters, char *ch, int *player_num, int **score, int *shown_cnt)
+void check_char(const char *word, int *word_len, int *shown_letters, char *ch, int *curr_player, int *score, int *shown_cnt)
 {
     for(int i=0; i<*word_len; i++)
     {
         if(!*(shown_letters+i) && *(word+i) == *ch) 
         {
-            *(score+*player_num) += 1;
+            *(score+*curr_player) += 1;
             *(shown_letters+i) = 1;     
             *shown_cnt += 1;
         }
@@ -98,7 +98,7 @@ void check_char(const char *word, int *word_len, int *shown_letters, char *ch, i
     return;
 }
 
-void print_UI(const char *word, int *word_len, int *shown_letters, int *player_num, char **names, int **score)
+void print_UI(const char *word, int *word_len, int *shown_letters, int *player_num, char **names, int *score)
 {
     for(int i=0; i<*word_len; i++)
     {
@@ -110,22 +110,71 @@ void print_UI(const char *word, int *word_len, int *shown_letters, int *player_n
 
     for(int i=0; i<*player_num; i++)
     {
-        printf("%s: %d\n", *(names+i), *(*score+i));
+        printf("%s: %d\n", *(names+i), *(score+i));
     }
     
     return;
 }
 
-void restart(const char *word, int *word_len, int *player_num, int **score)
+void restart(const char **word, int *word_len, int *score, int *shown_letters, int *shown_cnt)
 {
-    word = generate_word();
-    *word_len = strlen(word);
+    *shown_cnt = 0;
+    *word = generate_word();
+    *word_len = strlen(*word);
 
-    for(int i=0; i<*player_num; i++)
+    for(int i=0; i<MAX_WORD_LEN; i++)
     {
-        *(*score+i) = 0;
+        *(shown_letters+i) = 0;
     }
 
+    for(int i=0; i<MAX_PLAYER_NUM; i++)
+    {
+        *(score+i) = 0;
+    }
+
+    return;
+}
+
+void end_game(int *player_num, char **player_names, int *score)
+{
+    int *processed = malloc(MAX_PLAYER_NUM*sizeof(int));
+    int processed_cnt = 0;
+    
+    for(int i=0; i<*player_num; i++)
+    {
+        *(processed+i) = 0;
+    }
+
+    int max_score = 0;
+
+    printf("Score:\n");
+
+    while(processed_cnt<*player_num)
+    {
+        max_score = 0;
+
+        for(int i=0; i<*player_num; i++)
+        {
+            if(!*(processed+i) && *(score+i) > max_score)
+            {
+                max_score = *(score+i);
+            }
+        }
+
+        for(int i=0; i<*player_num; i++)
+        {
+            if(*(score+i) == max_score)
+            {
+                printf("%d. %s\n", ++processed_cnt, *(player_names+i));
+                *(processed+i) = 1;
+                break;
+            }
+        }
+    }
+    
+
+
+    free(processed);
 
     return;
 }
@@ -136,7 +185,7 @@ void wheel_of_fortune(void)
     const char *word = malloc(MAX_WORD_LEN*sizeof(char)); // word for players to guess
     int player_num; // number of active players
     int word_len; // size of word
-    int **score = malloc(MAX_PLAYER_NUM * sizeof(int *)); // score of every player
+    int *score = malloc(MAX_PLAYER_NUM * sizeof(int)); // score of every player
     int *shown_letters = malloc(MAX_WORD_LEN * sizeof(int)); // keeping track on which letters were shown
     int shown_cnt = 0; //keeping track of number of shown letters
     int curr_player = 0; // player making move in round
@@ -168,43 +217,41 @@ void wheel_of_fortune(void)
             return;
         }
 
-        *(score+i) = malloc(sizeof(int));
+        /**(score+i) = malloc(sizeof(int));
         if(!*(score+i))
         {
             perror("Memory Error");
             return;
-        }
+        }*/
     }
 
     enter_player_num(&player_num);
     enter_player_name(player_names, &player_num);
 
-    //restart(word, &word_len, &player_num, score);
-    
-    word = generate_word();
-    word_len = strlen(word);
+    restart(&word, &word_len, score, shown_letters, &shown_cnt);
 
     printf("%s\n", word);
 
-
-    while(shown_cnt <= word_len)
+    while(shown_cnt < word_len)
     {
         printf("\n");
 
         print_UI(word, &word_len, shown_letters, &player_num, player_names, score);
         enter_char(*(player_names+curr_player), &ch);
-        check_char(word, &word_len, shown_letters, &ch, &player_num, score, &shown_cnt);
+        check_char(word, &word_len, shown_letters, &ch, &curr_player, score, &shown_cnt);
 
         curr_player = (curr_player+1) % player_num;
     }
 
+    printf("\n%s\nCongratulations!\n", word);
 
-    
+    end_game(&player_num, player_names, score);
+
 
     for(int i=0; i<MAX_PLAYER_NUM; i++)
     {
         free(*(player_names+i));
-        free(*(score+i));
+        //free(*(score+i));
     }
 
     free(player_names);
